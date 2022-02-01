bbf93e8a0fec5ea3fc4a81e34bc9eb04f6b6ede8
Dayana Sánchez <dayana.sanchez@correo.unimet.edu.ve>
2022-01-31 20:41:57 -0400

    creación repositorio y gitignore

---
+++ src/analisisredessociales/AnalisisRedesSociales.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package analisisredessociales;
+
+/**
+ *
+ * @author dayan
+ */
+public class AnalisisRedesSociales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
